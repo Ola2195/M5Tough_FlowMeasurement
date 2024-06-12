8f5a4bd194127e05448cf1e43d8b13512741904e
Aleksandra Matysik <86144868+Ola2195@users.noreply.github.com>
2024-06-12 11:17:02 +0200

    Update M5Tough_FlowMeasurement.ino

--- M5Tough_FlowMeasurement.ino
+++ M5Tough_FlowMeasurement.ino
@@ -1,20 +1,20 @@
-#include <M5Unified.h>
 #include <Arduino.h>
+#include <M5Unified.h>
 
 #define DISPLAY_WIDTH   320
 #define DISPLAY_HEIGH   240
 
-hw_timer_t *timer = NULL;
-const int sensortPin = 36;  // Pin z przepływomierzem
-portMUX_TYPE interruptMux = portMUX_INITIALIZER_UNLOCKED;
+#define SENSOR_PIN    2     // Pin z przepływomierzem
 
-const double calibration = 1.0;
+hw_timer_t *timer = NULL;
+const int sensortPin = SENSOR_PIN;
+portMUX_TYPE interruptMux = portMUX_INITIALIZER_UNLOCKED;
 
 volatile int timeCount = 0;
 volatile int pulseCount = 0;
-volatile float flowRate = 0;
+volatile float frequency = 0.0;
 
-volatile bool cleraScreen = false;
+volatile float clearScreen = false;
 
 /*
  * Funkcja przerwania timera
@@ -22,12 +22,6 @@ volatile bool cleraScreen = false;
 void IRAM_ATTR onTimer() {
   portENTER_CRITICAL_ISR(&interruptMux);  // zabezpieczenie współdzielonych zmiennych
   timeCount++;
-  if(timeCount == 60) {
-    flowRate = pulseCount * calibration; 
-    pulseCount = 0;
-    timeCount = 0;
-    cleraScreen = true;
-  }
   portEXIT_CRITICAL_ISR(&interruptMux);
 }
 
@@ -38,6 +32,14 @@ void IRAM_ATTR pulseCounter() {
   portENTER_CRITICAL_ISR(&interruptMux);  // zabezpieczenie współdzielonych zmiennych
   pulseCount++;
   portEXIT_CRITICAL_ISR(&interruptMux);
+}
+
+void reset() {
+  if(timeCount!=0)
+    M5.Lcd.fillRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGH-40-20, BLACK);
+  timeCount = 0;
+  pulseCount = 0;
+  frequency = 0.0;
 }
 
 void setup() {
@@ -51,34 +53,46 @@ void setup() {
   // Konfiguracja GPIO
   pinMode(sensortPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(sensortPin), pulseCounter, FALLING);  
+  
+  // Rysowanie przycisku resetu na ekranie
+  M5.Lcd.fillRect(20, DISPLAY_HEIGH-40-20, DISPLAY_WIDTH-40, 40, TFT_WHITE);
+  M5.Lcd.setTextColor(TFT_BLACK);
+  M5.Lcd.setCursor((DISPLAY_WIDTH)/2-50, DISPLAY_HEIGH-40-10);
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.print("RESET");
 }
 
 void loop() {
-  if(cleraScreen) {
-    M5.Lcd.clear(BLACK);
-    cleraScreen = false;
-  }
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextColor(WHITE, BLACK);
+  M5.Lcd.setCursor(0, 0);
 
   char buffer[20];
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.setTextSize(3);
-
-  snprintf(buffer, sizeof(buffer), "Time: %i%i sec", timeCount/10, timeCount%10);
+  
+  snprintf(buffer, sizeof(buffer), "Time: %i sec", timeCount);
   M5.Lcd.println(buffer);
   
-  snprintf(buffer, sizeof(buffer), "Flow: %i L\n\r", pulseCount);
+  snprintf(buffer, sizeof(buffer), "Pulses: %i\n", pulseCount);
   M5.Lcd.println(buffer);
 
-  M5.Lcd.setTextSize(3);
-  //M5.Lcd.println("Flow Rate");
-  M5.Lcd.drawCentreString("Flow Rate", 160, 80, &fonts::Font0);
-  
-  M5.Lcd.setTextSize(10);
-  snprintf(buffer, sizeof(buffer), "%.2f", flowRate);
-  //M5.Lcd.println(buffer);
-  M5.Lcd.drawCentreString(buffer, 160, 130, &fonts::Font0);
+  portENTER_CRITICAL(&interruptMux);
+  if (timeCount > 0) {
+    frequency = pulseCount / (float)timeCount;
+  }
+  portEXIT_CRITICAL(&interruptMux);
 
-  M5.Lcd.setTextSize(3);
-  //M5.Lcd.println("L/min");
-  M5.Lcd.drawCentreString("L/min", 160, 200, &fonts::Font0);
+  M5.Lcd.setTextSize(5);
+  snprintf(buffer, sizeof(buffer), "%.2f Hz", frequency);
+  M5.Lcd.println(buffer);
+
+  // Sprawdzenie dotknięcia ekranu
+  M5.update();
+  if (M5.Touch.getCount() > 0) {
+    auto detail = M5.Touch.getDetail();
+    int x = detail.x;
+    int y = detail.y;
+    if (x >= 20 && x <= DISPLAY_WIDTH-20 && y >= DISPLAY_HEIGH-40-20 && y <= DISPLAY_HEIGH-20) {
+      reset();
+    }
+  }
 }
