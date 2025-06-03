1f9a483775dd6d69064d1ad58fedb13a7c1c482c
Ola2195 <tysiaczka195olas@gmail.com>
2025-06-03 13:44:02 +0200

    Organizacja TouchButton

--- include/TouchButton.h
+++ include/TouchButton.h
@@ -2,22 +2,71 @@
 #define TOUCHBUTTON_H
 
 #include <Arduino.h>
-#include <M5Unified.h>
 
+/**
+ * Klasa reprezentujaca przycisk dotykowy
+ * Odpowiada za rysowanie, wykrywanie klikniec i udostepnianie pozycji
+ */
 class TouchButton {
   public:
+    /**
+     * Konstruktor przycisku dotykowego
+     * @param label       Tekst etykiety przycisku
+     * @param x           Wspolrzedna X lewego gornego rogu
+     * @param y           Wspolrzedna Y lewego gornego rogu
+     * @param width       Szerokosc przycisku
+     * @param height      Wysokosc przycisku
+     * @param color       Kolor tla przycisku (domyslnie TFT_WHITE)
+     * @param textColor   Kolor tekstu (domyslnie TFT_BLACK)
+     */
     TouchButton(const String& label,
                 int x, int y, int width, int height,
                 uint16_t color = TFT_WHITE, uint16_t textColor = TFT_BLACK);
 
+    /**
+     * Rysuje przycisk na ekranie
+     */
     void draw();
+
+    /**
+     * Sprawdza, czy przycisk jest aktualnie wcisniety
+     * @param touchX  Pozycja dotyku w osi X
+     * @param touchY  Pozycja dotyku w osi Y
+     * @return true jesli dotyk znajduje sie w obszarze przycisku
+     */
     bool isPressed(int touchX, int touchY) const;
+
+    /**
+     * Sprawdza, czy przycisk zostal wcisniety jednokrotnie.
+     * @param touchX  Pozycja dotyku w osi X
+     * @param touchY  Pozycja dotyku w osi Y
+     * @return true jesli klikniecie nastapilo teraz (a nie bylo aktywne wczesniej)
+     */
     bool wasJustPressed(int touchX, int touchY);
 
+    /**
+     * Zwraca etykiete przycisku
+     */
     String getLabel() const;
+    
+    /**
+     * Zwraca wspolrzedna X przycisku
+     */
     int getX() const;
+
+    /**
+     * Zwraca wspolrzedna Y przycisku
+     */
     int getY() const;
+
+    /**
+     * Zwraca szerokosc przycisku
+     */
     int getWidth() const;
+
+    /**
+     * Zwraca wysokosc przycisku
+     */
     int getHeight() const;
 
   private:
@@ -25,7 +74,7 @@ class TouchButton {
     int _x, _y, _width, _height;
     uint16_t _color, _textColor;
 
-    bool _wasPreviouslyPressed = false; 
+    bool _wasPreviouslyPressed; ///< Flaga wykrywania klikniecia jednokrotnego
 };
 
 #endif // TOUCHBUTTON_H
--- src/TouchButton.cpp
+++ src/TouchButton.cpp
@@ -1,13 +1,16 @@
 #include "include/TouchButton.h"
 
+#include <M5Unified.h>
+
 TouchButton::TouchButton(const String& label, int x, int y, int width, int height, uint16_t color, uint16_t textColor)
-  : _label(label), _x(x), _y(y), _width(width), _height(height), _color(color), _textColor(textColor) {}
+  : _label(label), _x(x), _y(y), _width(width), _height(height), _color(color), _textColor(textColor),
+    _wasPreviouslyPressed(false) {}
 
 void TouchButton::draw() {
   M5.Lcd.fillRect(_x, _y, _width, _height, _color);
   M5.Lcd.setTextColor(_textColor);
   M5.Lcd.setTextSize(3);
-  int textX = _x + (_width - _label.length() * 18) / 2;  // Rough centering
+  int textX = _x + (_width - _label.length() * 18) / 2;
   int textY = _y + (_height - 24) / 2;
   M5.Lcd.setCursor(textX, textY);
   M5.Lcd.print(_label);
